337f224c88a895a975dcf397ba4fb34b306fe313
Patricia Alonso Ayllón <alonso.aypat22@triana.salesianos.edu>
2024-03-20 11:59:00 +0100

    afñksfjk

---
+++ EjerciciosTema6/src/ejercicio4/ExcepcionSumaMayor10.java
@@ -0,0 +1,9 @@
+package ejercicio4;
+
+public class ExcepcionSumaMayor10 extends Exception{
+	
+	public ExcepcionSumaMayor10(String msg) {
+		super(msg);
+	}
+
+}
---
+++ EjerciciosTema6/src/ejercicio4/Main.java
@@ -0,0 +1,21 @@
+package ejercicio4;
+
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/*
+		 * 4) Crea un programa que use dos variables, a y b. Si la suma de estas dos variables es mayor que 10, al ejecutarse
+			el programa debe aparecer una excepción que diga "La suma no puede ser mayor que 10". Por lo tanto, dale
+			valores a "a" y a "b" para que su suma sea superior a 10.
+		 */
+		
+		Scanner sc=new Scanner(System.in);
+		String aux;
+		double a,b,limiteSuma;
+
+	}
+
+}
